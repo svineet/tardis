e00a30ce808a4980460305151ce3da07654faf4d
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-29 21:40:21 +0000

    Update docs after building Travis build 3305 of tardis-sn/tardis
    The docs were built from the branch 'master' against the commit
    fd57afc26546a9ae9c7fbe3d2c7b14e794c7b560.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/tardis-sn/tardis/jobs/526172172.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/tardis/bin/doctr deploy --built-docs docs/_build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.dev64',
+    VERSION: '3.0.dev63',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
